2cedbc902a493bdfa79d43fb2e0eba12ca215d0c
MMMJB <michaelbeck445@gmail.com>
2023-08-21 21:01:09 -0400

    basic collapsed sidebar

--- src/Components/Sidebar/SidebarItem.jsx
+++ src/Components/Sidebar/SidebarItem.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 import { IonIcon } from "@ionic/react";
 
-export default function SidebarItem({ icon, name }) {
+export default function SidebarItem({ icon, name, collapsed }) {
   return (
     <li className="text-sidebar-text flex cursor-pointer items-center gap-6 py-4">
-      <IonIcon icon={icon} className="text-sidebar-icon text-2xl" />
-      <span className="font-roboto text-sm">{name}</span>
+      <IonIcon
+        icon={icon}
+        className={`${collapsed ? "mx-auto" : ""} text-sidebar-icon text-2xl`}
+      />
+      {!collapsed && <span className="font-roboto text-sm">{name}</span>}
     </li>
   );
 }
